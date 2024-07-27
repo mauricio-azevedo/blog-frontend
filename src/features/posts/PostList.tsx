2495e56f78ac80aff1f7c98611989997d7e078b5
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-07-27 13:03:55 -0300

    Set max-width for PostList container
    Added a max-width of 100% to ensure the PostList container scales properly on different screen sizes. This change enhances the responsiveness and visual consistency of the post list layout.

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -304,7 +304,7 @@ const PostList: React.FC = () => {
 
   return (
     <Flex vertical style={{ overflow: 'auto', alignItems: 'center' }}>
-      <Flex vertical style={{ minWidth: 220, width: 600 }}>
+      <Flex vertical style={{ minWidth: 220, width: 600, maxWidth: '100%' }}>
         <Flex justify="space-between" gap=".25rem" style={{ marginBottom: '16px' }}>
           <Button type="primary" onClick={showCreatePostModal} disabled={isFetching || isCommenting}>
             Create Post
